27ad647b2426f7d27d9b2a49e7e4de7aa083a5fd
0xBAMA <jon.baker1222@gmail.com>
2019-04-08 21:25:19 -0400

    basics

---
+++ cs_2/cs_2_client.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+
+#define MSG_LENGTH 100 //maximum length of message
+
+
+//expresses the type of the incoming message
+typedef enum {
+	REGULAR,
+	COMMAND
+} msg_type_t;
+
+
+//structure for the message
+typedef struct msg {
+
+	msg_type_t type;
+	int PID;
+	char message_text[MSG_LENGTH];
+
+} msg_t; //<--allows declaration with 'msg_t' rather than 'struct msg'
+
+
+
+
+int main()
+{
+	return 0;
+}
