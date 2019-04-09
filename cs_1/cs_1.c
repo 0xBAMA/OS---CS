a44174ab856fb8c74669a41b269d4b61bb42bda8
0xBAMA <jon.baker1222@gmail.com>
2019-04-08 20:11:37 -0400

    checking in cs1 code

--- cs_1/cs_1.c
+++ cs_1/cs_1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <string.h>
 
 
 #define MSG_LENGTH 100 //maximum length of message
@@ -22,24 +22,77 @@ typedef struct msg {
 } msg_t; //<--allows declaration with 'msg_t' rather than 'struct msg'
 
 
+
 int main(){
 
 	int child_PID;
 	int parent_PID = getpid(); //getpid() for pid, getppid() for parent pid
 
 	//set up pipe
+	int cs_pipe[2];
+	pipe(cs_pipe);
 
-
+//******************************************************************************
 	//get the child's PID at the same time as the fork
-	if((child_PID = fork())){//parent process
-		//close write end then read messages
+	if((child_PID = fork())){//parent process - closes write end then read messages
+		close(cs_pipe[1]); //close write end of the pipe
 
+		msg_t recieved_message;
 
-	}else{						//child process
-		//close read end then send messages
+		//sleep(5);
 
+		while(1)
+		{
+			read(cs_pipe[0],(char*)&recieved_message,sizeof(msg_t));
+			sleep(1);
 
+			if(recieved_message.type == REGULAR)
+			{//print the string
+				printf("\nRecieved string: \n %sfrom client with PID %d\n\n",recieved_message.message_text, child_PID);
+			}
+
+			if(recieved_message.type == COMMAND)
+			{//exit the process
+				printf("Exit signal recieved, goodbye\n");
+				break;
+			}
+		}
+
+//******************************************************************************
+	}else{						//child process - closes read end then send messages
+		close(cs_pipe[0]); //close read end of the pipe
+
+		char chlid_message_string[MSG_LENGTH]; //holds the user input
+		char buf[6];
+		char exit_string[5] = "exit";
+		char send_string[5] = "send";
+
+		msg_t msg;
+
+		printf("This is the child - enter \'send:\' followed by a message to have the server print, or enter \'exit\' to exit.\n");
+
+		while(1)
+		{
+			fgets(chlid_message_string,MSG_LENGTH,stdin);//get the input
+			sprintf(buf,"%.4s",chlid_message_string);//grab the first four characters
+
+			if(!strcmp(buf,exit_string))
+			{
+				sprintf(msg.message_text, "%s", chlid_message_string);
+				msg.type = COMMAND;
+
+				break;//exit the infinite while loop, if the user input started with "exit"
+			}
+
+			if(!strcmp(buf,send_string))
+			{//we know the string starts with send - construct the message and send it
+				sprintf(msg.message_text, "%s", chlid_message_string+6); //drop "send: "
+				msg.type = REGULAR;
+
+				write(cs_pipe[1],(char*)&msg,sizeof(msg_t));
+			}
+		}
 	}
-
+//******************************************************************************
 	return 0;
 }
